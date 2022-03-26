9c0ad1a36b01d447cdc67096a8cb76ba75607a4d
Onur <onurbelek@outlook.com>
2022-03-26 10:31:25 +0000

    passed all the tests for question 4 solution is complete!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreParams) {
+    return array.filter(element => !moreParams.includes(element));
+    
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
