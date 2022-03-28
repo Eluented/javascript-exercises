736f1d5f043628e28e6abac3bd6997197af7c965
Onur <onurbelek@outlook.com>
2022-03-28 10:26:54 +0100

    adding my solution and passing all the tests for question 5 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum = 0;
+    largeNum = 0;
+    smallNum = 0;
 
+    if (num1 < 0 || num2 < 0 || Number.isInteger(num1) === false || Number.isInteger(num2) === false){
+        return 'ERROR';
+    } else {
+        if (num1 > num2){
+            largeNum = num1;
+            smallNum = num2;
+        } else {
+            largeNum = num2;
+            smallNum = num1;
+        }
+        for (let i = smallNum; i <= largeNum; i++){
+            sum += i;
+        } 
+        return sum;
+    }
 };
 
 // Do not edit below this line
