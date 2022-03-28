4147d91f11bc935ec5bd95961ab340294a9098fe
Onur <onurbelek@outlook.com>
2022-03-28 18:13:19 +0100

    passed all the tests for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let equation = (farenheit - 32) * 5/9;
+  return Math.round(equation * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let equation = (celsius * 9/5) + 32;
+  return Math.round(equation * 10) / 10;
 };
 
 // Do not edit below this line
